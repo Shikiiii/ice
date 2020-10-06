aa204d43273a1f2f0eb71f1b666dfe37147fe197
i suck at coding <33571437+Shikiiii@users.noreply.github.com>
2020-10-06 10:04:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -150,7 +150,7 @@ async def on_message(m):
 			async for message in trigger_chan.history(limit=None):
 				if message.content == "reset":
 					temp_numb_counter += 1
-				if temp_numb_counter == 2
+				if temp_numb_counter == 2:
 					countmessages = False
 					print("Starting resetting...")
 					a = fetch_top_members_msg()
