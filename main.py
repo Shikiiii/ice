a17a20a4429f39ff6d007827c28f75e71d246a0f
i suck at coding <33571437+Shikiiii@users.noreply.github.com>
2020-10-06 09:13:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ async def on_ready():
 	trigger_chan = bot.get_channel(728009012028768257)
 	log = bot.get_channel(729058195330433094)
 	lb_channel = bot.get_channel(760418426455195668)
-	lb_msg = await lb_channel.fetch_message(762548536553635840
+	lb_msg = await lb_channel.fetch_message(762548536553635840)
 	ice = bot.get_guild(734867158475079690)
 	temp_numb_counter = 0
 	async for message in trigger_chan.history(limit=None):
